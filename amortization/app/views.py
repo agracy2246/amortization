c058feddb0ebebeba1d3431564f55bb48107a791
Bobby D <adamg@aol.com>
2020-06-25 07:50:20 -0500

    stuff

--- amortization/app/views.py
+++ amortization/app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+import sys
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from  import Amortization
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    am = amortization(120000, .03, 30)
+    am.saveReport()
+    return render(request, 'app/index.html')
