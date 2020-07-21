22282bab3df5bfb7947e96e1d3fb43a82a0ffef9
Bobby D <adamg@aol.com>
2020-07-21 06:05:15 -0500

    added the extra payment textfields

--- amortization/app/views.py
+++ amortization/app/views.py
@@ -8,19 +8,34 @@ from .tablerow       import TableRow
 
 # Create your views here.
 def index(request):
-    am = Amortization(250000, .02, 30)
-    am.generateSchedule()
     
-    
-    rowData = []
-    loanMonths = am.info['loanYears'] * 12
-    for i in range(loanMonths):
-        row = TableRow(i+1, am.info['interestList'][i], am.info['principalList'][i], am.info['loanBalanceList'][i])
-        rowData.append(row)
-       
 
-    data = {
-        'loanAmount': am.info['loanAmount'],
-        'rowData'   : rowData
-    }
-    return render(request, 'app/index.html', context=data)
+    if request.method == "GET":
+        return render(request, 'app/index.html')    
+
+    elif request.method == "POST":
+        
+           
+        loanAmount = int(request.POST['loanAmount'])
+        loanYears  = int(request.POST['loanYears'])
+        yearlyInterestDecimal = float(request.POST['yearlyInterestPercent']) / 100.0
+        print(type(loanAmount))
+        print(type(loanYears))
+
+        am = Amortization(loanAmount, yearlyInterestDecimal, loanYears)
+        am.generateSchedule()
+        
+        
+        rowData = []
+        loanMonths = am.info['loanYears'] * 12
+        for i in range(loanMonths):
+            row = TableRow(i+1, am.info['interestList'][i], am.info['principalList'][i], am.info['loanBalanceList'][i])
+            rowData.append(row)
+        
+
+        data = {
+            'loanAmount': am.info['loanAmount'],
+            'rowData'   : rowData
+        }
+
+        return render(request, 'app/index.html', context=data)
